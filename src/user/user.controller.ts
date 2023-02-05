09e76189c936614a4b9a3e4b38b7fe1d795cf5e7
vadim-mg <vadikm1981@yandex.ru>
2023-02-05 11:33:36 +0500

    feat: implement Users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,34 +1,68 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  HttpCode,
+  Put,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { HttpException } from '@nestjs/common/exceptions';
+import { HttpStatus } from '@nestjs/common/enums';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  async findAll() {
+    return await this.userService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    const result = await this.userService.findOne(id);
+    if (!result) {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
+    return result;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  @Put(':id')
+  async update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    console.log('======have find why test on 232 line broken!=====');
+    const changedUser = await this.userService.update(id, updatePasswordDto);
+    if (changedUser === 404) {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
+
+    if (changedUser === 403) {
+      throw new HttpException('FORBIDDEN', HttpStatus.FORBIDDEN);
+    }
+    return changedUser;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @HttpCode(204)
+  async remove(@Param('id', new ParseUUIDPipe()) id: string) {
+    try {
+      const deletedUser = await this.userService.remove(id);
+      return deletedUser;
+    } catch (err) {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { db } from 'src/utils/DB/db.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+import { UserEntity } from './entities/user.entity';
+import { hash as passHash, compare as passCompare } from 'bcrypt';
 
+const saltOrRounds = 10;
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private clearUser(user: UserEntity) {
+    if (user && user.password) {
+      delete user.password;
+    }
+    return user;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async create(createUserDto: CreateUserDto) {
+    createUserDto.password = await passHash(
+      createUserDto.password,
+      saltOrRounds,
+    );
+    return this.clearUser(await db.users.create(createUserDto));
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findAll() {
+    return (await db.users.findMany()).map(this.clearUser);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: string) {
+    return this.clearUser(await db.users.findOne({ key: 'id', equals: id }));
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: string, updatePasswordDto: UpdatePasswordDto) {
+    console.log('--------');
+    const foundUser = await db.users.findOne({ key: 'id', equals: id });
+    console.log(foundUser);
+    if (foundUser === null) {
+      return 404;
+    }
+    const isMatch = await passCompare(
+      updatePasswordDto.oldPassword,
+      foundUser.password,
+    );
+    if (!isMatch) {
+      return 403;
+    }
+    const changeUserDto = {
+      password: await passHash(updatePasswordDto.newPassword, saltOrRounds),
+      updatedAt: new Date().getTime(),
+      version: foundUser.version + 1,
+    };
+    return this.clearUser(await db.users.change(id, changeUserDto));
+  }
+
+  async remove(id: string) {
+    return this.clearUser(await db.users.delete(id));
   }
 }
