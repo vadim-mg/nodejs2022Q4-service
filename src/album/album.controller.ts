73b9b8f8110cb2144a8fcfd4cebfa8be34b98f80
vadim-mg <vadikm1981@yandex.ru>
2023-02-05 18:48:48 +0500

    feat: implement album endpoints

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,34 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  Put,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 
 @Controller('album')
 export class AlbumController {
-  constructor(private readonly albumService: AlbumService) {}
+  constructor(private readonly albumService: AlbumService) { }
 
   @Post()
-  create(@Body() createAlbumDto: CreateAlbumDto) {
-    return this.albumService.create(createAlbumDto);
+  async create(@Body() createAlbumDto: CreateAlbumDto) {
+    return await this.albumService.create(createAlbumDto);
   }
 
   @Get()
-  findAll() {
-    return this.albumService.findAll();
+  async findAll() {
+    return await this.albumService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.albumService.findOne(+id);
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    const result = await this.albumService.findOne(id);
+    if (!result) {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
+    return result;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAlbumDto: UpdateAlbumDto) {
-    return this.albumService.update(+id, updateAlbumDto);
+  @Put(':id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() updateAlbumDto: UpdateAlbumDto,
+  ) {
+    return await this.albumService.update(id, updateAlbumDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.albumService.remove(+id);
+  @HttpCode(204)
+  async remove(@Param('id', new ParseUUIDPipe()) id: string) {
+    try {
+      const deletedAlbum = await this.albumService.remove(id);
+      return deletedAlbum;
+    } catch {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
   }
 }
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { db } from 'src/utils/DB/db.service';
 
 @Injectable()
 export class AlbumService {
-  create(createAlbumDto: CreateAlbumDto) {
-    return 'This action adds a new album';
+  private async isArtistIdValid(id: string) {
+    if (id && !(await db.artists.findOne({ key: 'id', equals: id })))
+      throw new HttpException(
+        `BAD_REQUEST: artistID(${id}) does not exist!`,
+        HttpStatus.BAD_REQUEST,
+      );
+    return true;
   }
 
-  findAll() {
-    return `This action returns all album`;
+  async create(createAlbumDto: CreateAlbumDto) {
+    if (await this.isArtistIdValid(createAlbumDto.artistId)) {
+      return await db.albums.create(createAlbumDto);
+    }
+    return null;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} album`;
+  async findAll() {
+    return await db.albums.findMany();
   }
 
-  update(id: number, updateAlbumDto: UpdateAlbumDto) {
-    return `This action updates a #${id} album`;
+  async findOne(id: string) {
+    return await db.albums.findOne({ key: 'id', equals: id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} album`;
+  async update(id: string, updateAlbumDto: UpdateAlbumDto) {
+    const foundAlbum = await db.albums.findOne({ key: 'id', equals: id });
+    if (!foundAlbum) {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
+    return await db.albums.change(id, updateAlbumDto);
+  }
+
+  async remove(id: string) {
+    return await db.albums.delete(id);
   }
 }
