fb57aff4d16aa69d8e4c9fbfb1b1b9e862350e54
vadim-mg <vadikm1981@yandex.ru>
2023-02-06 20:17:02 +0500

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 
@@ -8,6 +13,22 @@ const PORT = process.env.port ?? 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Library IPI')
+    .setDescription('The library API description')
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    ignoreGlobalPrefix: true,
+    deepScanRoutes: true,
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('doc', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       enableDebugMessages: true,
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,10 +1,21 @@
 // export class User {}
+import { ApiProperty } from '@nestjs/swagger';
 
-export type UserEntity = {
+export class UserEntity {
   id: string; // uuid v4
+
+  @ApiProperty({
+    description: 'User login',
+    type: 'string',
+  })
   login: string;
+
+  @ApiProperty()
   password: string;
+
   version: number; // integer number, increments on update
+
   createdAt: number; // timestamp of creation
+
   updatedAt: number; // timestamp of last update
 };
