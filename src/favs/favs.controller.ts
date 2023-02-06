aeee8def6c88913a65d1582ba0d7f5e31037acba
vadim-mg <vadikm1981@yandex.ru>
2023-02-06 14:02:45 +0500

    feat: implement favs endpoints

--- src/favs/favs.controller.ts
+++ src/favs/favs.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  ParseEnumPipe,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { StatusCodes } from 'http-status-codes';
+import { FavTypes } from './entities/fav.entity';
 import { FavsService } from './favs.service';
-import { CreateFavDto } from './dto/create-fav.dto';
-import { UpdateFavDto } from './dto/update-fav.dto';
 
 @Controller('favs')
 export class FavsController {
-  constructor(private readonly favsService: FavsService) {}
-
-  @Post()
-  create(@Body() createFavDto: CreateFavDto) {
-    return this.favsService.create(createFavDto);
-  }
+  constructor(private readonly favsService: FavsService) { }
 
   @Get()
-  findAll() {
-    return this.favsService.findAll();
+  async getAll() {
+    return await this.favsService.getAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.favsService.findOne(+id);
+  @Post('/:type/:id')
+  async addFav(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Param('type', new ParseEnumPipe(FavTypes)) type: FavTypes,
+  ) {
+    return await this.favsService.add({ type, id });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFavDto: UpdateFavDto) {
-    return this.favsService.update(+id, updateFavDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.favsService.remove(+id);
+  @Delete('/:type/:id')
+  @HttpCode(StatusCodes.NO_CONTENT)
+  async removeFav(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Param('type', new ParseEnumPipe(FavTypes)) type: FavTypes,
+  ) {
+    try {
+      return await this.favsService.remove(id, type);
+    } catch {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
   }
 }
--- src/favs/favs.service.ts
+++ src/favs/favs.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateFavDto } from './dto/create-fav.dto';
-import { UpdateFavDto } from './dto/update-fav.dto';
+import { db } from 'src/utils/DB/db.service';
+import { FavTypes } from './entities/fav.entity';
 
+const favTypeToDbName = (name: string) => name + 's';
 @Injectable()
 export class FavsService {
-  create(createFavDto: CreateFavDto) {
-    return 'This action adds a new fav';
+  async getAll() {
+    const allFavs = {};
+    allFavs[favTypeToDbName(FavTypes.artist)] = [];
+    allFavs[favTypeToDbName(FavTypes.album)] = [];
+    allFavs[favTypeToDbName(FavTypes.track)] = [];
+
+    const all = await db.favs.findMany();
+
+    for (const item of all) {
+      const found = await db[favTypeToDbName(item.type)].findOne({
+        key: 'id',
+        equals: item.id,
+      });
+      if (found) {
+        allFavs[favTypeToDbName(item.type)].push(found);
+      }
+    }
+
+    return allFavs;
   }
 
-  findAll() {
-    return `This action returns all favs`;
+  async add(createFavDto: CreateFavDto) {
+    const found = await db[favTypeToDbName(createFavDto.type)].findOne({
+      key: 'id',
+      equals: createFavDto.id,
+    });
+
+    if (!found) {
+      throw new HttpException(
+        `UNPROCESSABLE_ENTITY: ${createFavDto.type} ${createFavDto.id} doesn't exist`,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    return await db.favs.create(createFavDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} fav`;
-  }
-
-  update(id: number, updateFavDto: UpdateFavDto) {
-    return `This action updates a #${id} fav`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} fav`;
+  async remove(id: string, type: FavTypes) {
+    return await db.favs.delete(id);
   }
 }
