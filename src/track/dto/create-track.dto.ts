8f6c8be3dacaf2b074473197510ef113c833924f
vadim-mg <vadikm1981@yandex.ru>
2023-02-20 14:37:49 +0500

    feat:  describe swagger for track

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsString,
@@ -6,16 +7,20 @@ import {
   IsNumber,
 } from 'class-validator';
 export class CreateTrackDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   name: string;
-
+  
+  @ApiProperty()
   @ValidateIf((_object, value) => value === null || isUUID(value))
   artistId: string | null; // refers to Artist
-
+  
+  @ApiProperty()
   @ValidateIf((_object, value) => value === null || isUUID(value))
   albumId: string | null; // refers to Album
-
+  
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   duration: number; // integer number
--- src/track/entities/track.entity.ts
+++ src/track/entities/track.entity.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class TrackEntity {
+  @ApiProperty({ format: 'UUID', required: true })
   id: string; // uuid v4
+
+  @ApiProperty()
   name: string;
+
+  @ApiProperty()
   artistId: string | null; // refers to Artist
+
+  @ApiProperty()
   albumId: string | null; // refers to Album
+
+  @ApiProperty()
   duration: number; // integer number
 }
