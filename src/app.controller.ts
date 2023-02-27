91f7e882050ab936864a2a9b768da9850b8d0c44
vadim-mg <vadikm1981@yandex.ru>
2023-02-28 03:02:41 +0500

    fix: link to doc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Post, Request } from '@nestjs/common';
+import { Controller, Get, Post, Request } from '@nestjs/common';
 import { Public } from './auth/auth.decorator';
 import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @Public()
   @Post('auth/login')
   async login(@Request() req) {
     return await this.authService.login(req.body);
+  }
+
+  @Public()
+  @Get()
+  getHello(): string {
+    return '<a href="/doc">API DOC</a>';
   }
 }
